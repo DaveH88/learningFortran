2bbeabc1c6a3f89e3ca26a23c4dbd9373c8965f3
Dave Holland <dholland947@gmail.com>
2015-09-19 20:43:05 +0100

    create logical expression test file

---
+++ logicalExp.f90
@@ -0,0 +1,16 @@
+program logicalExp
+      implicit none
+
+      real::val1,val2
+      logical::result
+
+      val1 = -3.5
+      val2 =  2.0
+
+      result = val1 < val2
+      result = val1 >= val2
+      result = val1 < (val2 - 2.0)
+end program logicalExp
+
+
+
